4db2614ce07de14707f0d617c07e69eef7e242eb
5hade5layer <bharath218usba@gmail.com>
2020-05-28 17:07:25 +0530

    image input part removed

--- mysite/simulation/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Simulation
-
-class SimulationForm(forms.ModelForm):
-    class Meta:
-        model = Simulation
-        fields = ['cover']
--- mysite/simulation/views.py
+++ mysite/simulation/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.views.generic import ListView, CreateView 
-from django.urls import reverse_lazy 
-from .forms import SimulationForm 
-from .models import Simulation
 from .heat import heat
 
-class home(CreateView): 
-    model = Simulation
-    form_class = SimulationForm
-    template_name = 'input.html'
-    success_url = reverse_lazy('result')
+def home(request): 
+    return render(request, 'input.html')
 
 def result(request):
     heat()
